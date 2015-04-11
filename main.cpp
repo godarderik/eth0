9331730ef790a2ccbd5ddb9b0b57a531c8e66298
Rachit Singh <rachitsingh@outlook.com>
2015-04-11 10:57:47 -0400

    added the market bot

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
-#include <iostream>
-#include <math.h>
+#include <iostream>    //cout
+#include <stdio.h> //printf
+#include <string.h>    //strlen
+#include <string>  //string
+#include <sys/socket.h>    //socket
+#include <arpa/inet.h> //inet_addr
+#include <netdb.h> //hostent
 
 using namespace std;
 
+class tcp_client
+{
+private:
+    int sock;
+    std::string address;
+    int port;
+    struct sockaddr_in server;
+     
+public:
+    tcp_client();
+    bool conn(string, int);
+    bool send_data(string data);
+    string receive(int);
+};
+ 
+tcp_client::tcp_client()
+{
+	// configure this value later
+    sock = -1;
+    port = 0;
+    address = "";
+}
+
 int main ()
 {
-
+	int sockfd, newsockfd, portno, clilen, n;
     cout << "Hello World" << endl;
     return 0;
 }
---
+++ main.py
@@ -0,0 +1,66 @@
+# twisted imports
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+from twisted.python import log
+import json
+
+# system imports
+import time, sys
+
+TEST_PUBLIC_IP = "52.1.34.5"
+TEST_PRIVATE_IP = "10.0.138.127"
+
+JSON_PORT = 25000
+SLOW_MARKET = 0
+FAST_MARKET = 1
+EMPTY_MARKET = 2
+
+class MarketBot(Protocol):
+    def connectionMade(self):
+    	# maybe do something here
+    	print("Connected.")
+
+    def connectionLost(self, reason):
+    	print("Disconnected for reason: {0}".format(reason))
+
+    def dataReceived(self, data):
+
+    def message(self, message):
+        self.transport.write(message + '\n')
+
+class MarketBotFactory(protocol.ClientFactory):
+    """
+    A factory for MarketBots.
+
+    A new protocol instance will be created each time we connect to the server.
+    """
+
+    def __init__(self, channel):
+        self.channel = channel
+        self.filename = filename
+
+    def buildProtocol(self, addr):
+        p = MarketBot()
+        p.factory = self
+        return p
+
+    def clientConnectionLost(self, connector, reason):
+        """If we get disconnected, reconnect to server."""
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "connection failed:", reason
+        reactor.stop()
+
+if __name__ == '__main__':
+    # initialize logging
+    log.startLogging(sys.stdout)
+    
+    # create factory protocol and application
+    f = MarketBotFactory(sys.argv[1])
+
+    # connect factory to this host and port
+    reactor.connectTCP(TEST_PUBLIC_IP, JSON_PORT + SLOW_MARKET, f)
+
+    # run bot
+    reactor.run()
